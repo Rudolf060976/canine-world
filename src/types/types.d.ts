fbce046323a40e15eb8826976cdc0d2477268776
Rafael Urbina <rafaelurbinan@hotmail.com>
2021-12-08 19:20:12 -0400

    Initial setup & project structure

---
+++ src/types/types.d.ts
@@ -0,0 +1,22 @@
+type BreedsResponseType = {
+  message: Record<string, string[]>;
+  status: string;
+}
+
+type SubBreedsResponseType = {
+  message: string[];
+  status: string;
+}
+
+type SubBreedImagesResponseType = {
+  message: string[];
+  status: string;
+}
+
+type SubBreedRandomImageResponseType = {
+  message: string;
+  status: string;
+}
+
+
+
